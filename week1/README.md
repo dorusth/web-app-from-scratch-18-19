fd2ca6953e0c26f573c3c607d68a9ac33a3c8748
dorusth <dorusth@hotmail.com>
2019-02-06 15:36:01 +0100

    Update README Changed data request to use Promise Last search is saved for next visist Changed templating/rendering format Added error catch for request error

--- week1/README.md
+++ week1/README.md
@@ -1,7 +1,47 @@
-# Week 1 - Hello API 🐒
+# Week 1 - GitHub api repos
 
-Doel: Data ophalen uit een API en renderen in een overzichtspagina.
 
-[Opdrachten](https://drive.google.com/open?id=1OVhWQNaCgSluYviTKKWcApkyPd23xow1PiExb8GYANM)
+<!-- Add a link to your live demo in GitHub Pages 🌐-->
+## Live demo
+[Live demo](https://dorusth.github.io/web-app-from-scratch-18-19/week1/) | [Repo](https://github.com/dorusth/web-app-from-scratch-18-19/tree/master/week1)
 
-[Slides](https://drive.google.com/open?id=1Rjl9xqXoKniQSRJPdkU1O5YwWC33SJK8KiV0a-H_xZU)
+<!-- ☝️ replace this description with a description of your own work -->
+## GitHub repos
+In this project I used the GitHub API to fetch data about a users public repo's.
+With this app you can search for users and get an overview of their work on GitHub.
+
+<!-- Add a nice image here at the end of the week, showing off your shiny frontend 📸 -->
+[image]
+
+<!-- Maybe a table of contents here? 📚 -->
+
+<!-- How about a section that describes how to install this project? 🤓 -->
+## Installation
+Run the [demo](https://dorusth.github.io/web-app-from-scratch-18-19/week1/) or download the files and open the index.html file
+
+<!-- ...but how does one use this project? What are its features 🤔 -->
+## Features
+- Find users
+- browse repo's
+- View repo's
+
+
+<!-- What external data source is featured in your project and what are its properties 🌠 -->
+## Data sources
+For this project the [GitHub API](https://developer.github.com/v3/) to get the data
+
+<!-- Maybe a checklist of done stuff and stuff still on your wishlist? ✅ -->
+## To-do / Done list
+- [x] Get data from api
+- [x] Use promise to request data
+- [x] Input to change user
+- [x] Render data to HTML
+- [x] style rendered data
+- [ ] research data to use
+- [ ] Add interactions
+- [ ] multiple pages
+- [ ] Routes
+- [ ] Add states
+- [ ] manipulate Data
+
+<!-- How about a license here? 📜 (or is it a licence?) 🤷 -->
--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,39 +1,52 @@
-'use strict'
+const 	element = document.querySelector("main"),
+		form = document.querySelector("form");
+let 	user = localStorage.name || "cmda-minor-web";
 
-var element = document.querySelector("main");
-var form = document.querySelector("form");
-var user = "wesbos"
-var request = new XMLHttpRequest();
+(()=>{
+	function requestData(){
+		var getData = new Promise(function(resolve, reject){
+			const request = new XMLHttpRequest();
+			let apiLink = "https://api.github.com/users/"+user+"/repos";
+			request.open('GET', apiLink, true);
 
+			request.onload = function () {
+				if (request.status >= 200 && request.status < 400) {
+			        const data = JSON.parse(request.responseText);
+			        resolve(data);
+				}
+			}
+			request.send();
+		});
 
-function getData(){
-	var apiLink = "https://api.github.com/users/"+user+"/repos";
-	request.open('GET', apiLink, true);
-
-	request.onload = function () {
-		var data =  JSON.parse(this.response);
-		element.innerHTML = "";
-		localStorage.setItem('gitData', JSON.stringify(data));
-		data.forEach(repo =>{
-			var template =`
-			<article>
-				<a href="${repo.html_url}">
-					<h2> ${repo.name}</h2>
-				 </a>
-				 <p>${repo.description}</p>
-			 </article>
-			`;
-			element.innerHTML += template;
+		getData.then(function(fromResolve) {
+			renderData(fromResolve);
+			localStorage.setItem('gitData', JSON.stringify(fromResolve));
 		})
 	}
+	form.addEventListener("submit", userChange);
+	function userChange(){
+		event.preventDefault();
+		user = form.querySelector("input[type=text]").value;
+		localStorage.name = form.querySelector("input[type=text]").value;
+		requestData();
+	}
+	requestData();
+})();
 
-	request.send();
+function renderData(data){
+	let template = function(repo){
+		return (`
+		<article>
+			<a href="${repo.html_url}">
+				<h2> ${repo.name}</h2>
+			 </a>
+			 <p>${repo.description}</p>
+		 </article>
+		 `)
+	};
+	element.innerHTML = "";
+	data.forEach(repo =>{
+		var renderTemplate = template(repo);
+		element.innerHTML += renderTemplate;
+	})
 }
-
-form.addEventListener("submit", (event)=>{
-	event.preventDefault();
-	user = form.querySelector("input[type=text]").value;
-	getData();
-})
-
-getData();
