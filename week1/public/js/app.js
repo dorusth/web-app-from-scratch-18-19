ba11e920a334862d144863f87dba9f444efe2a86
dorusth <dorusth@hotmail.com>
2019-02-15 10:58:59 +0100

    added routing added multiple pages and requests refactored code

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,7 +1,39 @@
 (()=>{
+	let apiLink = "";
+	window.addEventListener("hashchange", checkRoute);
+	checkRoute()
+
+
+	async function checkRoute(){
+		let data;
+		let route = window.location.hash;
+		switch (route) {
+			case ("" && "/#") :
+				apiLink = "https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-1819/forks";
+				data = await requestData(apiLink)
+				renders.repos(data);
+				break;
+			default:
+				apiLink = handleUser(route.split("#user/")[1]);
+				data = await requestData(apiLink)
+				renders.issues(data);
+				break;
+		}
+	}
+	function handleUser(route){
+		const data = JSON.parse(localStorage.gitData);
+		switch(route.split("/")[2]){
+			case "open":
+				return data[route.split("/")[1]].issues_url.slice(0, -9);
+			case "closed":
+				return data[route.split("/")[1]].issues_url.slice(0, -9) + "?state=closed";
+		}
+	}
+})();
+
+	function requestData(apiLink){
 		let getData = new Promise(function(resolve, reject){
 			const request = new XMLHttpRequest();
-			let apiLink = "https://api.github.com/repos/cmda-minor-web/web-app-from-scratch-1819/forks";
 			request.open('GET', apiLink, true);
 
 			request.onload = function () {
@@ -12,29 +44,47 @@
 			}
 			request.send();
 		});
-
-		getData.then(function(fromResolve) {
-			renderData(fromResolve);
-			localStorage.setItem('gitData', JSON.stringify(fromResolve));
-		})
+		return getData
 	}
-)();
 
-function renderData(data){
-	const element = document.querySelector("main");
-	let template = function(repo){
-		return (`
-		<article>
-			<a href="${repo.html_url}">
-				<h2> ${repo.name}</h2>
-			 </a>
-			 <p> User: ${repo.owner.login}</p>
-		 </article>
-		 `)
-	};
-	element.innerHTML = "";
-	data.forEach(repo =>{
-		var renderTemplate = template(repo);
-		element.innerHTML += renderTemplate;
-	})
-}
+	const renders = {
+		element: document.querySelector("main"),
+		repos(data){
+			let template = function(repo, i){
+				return (`
+					<article>
+						<a class="link" href="#user/${repo.owner.login}/${i}/open">
+							<h2> ${repo.name}</h2>
+						 </a>
+						 <p> User: ${repo.owner.login}</p>
+						 <p> last commit: ${repo.updated_at} </p>
+					 </article>
+				 `)
+			};
+
+			this.element.innerHTML = "";
+			data.forEach((repo,i) =>{
+				let renderTemplate = template(repo,i);
+				this.element.innerHTML += renderTemplate;
+			})
+		},
+		issues(data){
+			let template = function(repo, i){
+				return (`
+					<article>
+						<a class="link" href="${repo.html_url}">
+							<h2> ${repo.title}</h2>
+						 </a>
+						 <p> User: ${repo.user.login}</p>
+						 <p> last commit: ${repo.created_at} </p>
+					 </article>
+				 `)
+			};
+
+			this.element.innerHTML = "";
+			data.forEach((repo,i) =>{
+				let renderTemplate = template(repo,i);
+				this.element.innerHTML += renderTemplate;
+			})
+		}
+	}
